dee19de26e3e0c886bb89455c2c580a57ebaf63f
Mujakkir <mujakkirlucse@gmail.com>
2022-09-29 19:20:39 +0600

    update card

--- src/pages/Category.js
+++ src/pages/Category.js
@@ -8,11 +8,10 @@ const Category = () =>{
     const image3 = 'https://images.unsplash.com/photo-1527866512907-a35a62a0f6c5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjcyNTgzfQ&w=392&q=70&auto=format&fit=crop&h=496'
     return(
         <Box>
-            <Grid container spacing={2}>
+            <Grid container width="74%" mx="auto" spacing={1}>
                 <Grid item xs={12} sm={6} md={4}>
-                    <Stack justifyContent="center">
+                    <Stack alignItems="center">
                     <img
-                        width="80%"
                      src={`${image1}`}
                      />
                         <Button variant='mainButton' sx={{width:"40%"}}>Accessories</Button>
@@ -20,19 +19,17 @@ const Category = () =>{
                 </Grid>
 
                 <Grid item xs={12} sm={6} md={4}>
-                    <Stack>
+                    <Stack alignItems="center">
                     <img
-                        width="80%"
                         src={`${image2}`}
                     />
                         <Button variant='mainButton' sx={{width:"40%"}}>Clothes</Button>
                     </Stack>
                 </Grid>
 
-                <Grid item xs={12} sm={6} md={4}>
-                    <Stack>
+                <Grid  item xs={12} sm={6} md={4}>
+                    <Stack alignItems="center" >
                     <img
-                        width="80%"
                         src={`${image3}`}
                     />
                         <Button variant="mainButton" sx={{width:"40%"}} >Toys</Button>
